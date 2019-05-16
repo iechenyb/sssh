8fcb3bb8e036868f2bcd13b19a7217963020cdb6
iechenyb <zzuchenyb@sina.com>
2019-05-16 10:10:32 +0800

    commit

--- src/com/cyb/sssh/background/controller/AnoController.java
+++ src/com/cyb/sssh/background/controller/AnoController.java
@@ -1,12 +1,18 @@
 package com.cyb.sssh.background.controller;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//@EnableGlobalMethodSecurity(prePostEnabled=true)
 @Controller
 @Scope("prototype")
 @RequestMapping("/ano")
@@ -30,4 +36,10 @@ public class AnoController {
 	public String login(){
 		return "admin";
 	}	
+	@GetMapping("/{id}")
+	@PreAuthorize("hasRole('ROLE_VOL') or hasRole('ROLE_ORG')")//是否具备其中一个角色
+	public ResponseEntity<?> cacheable(@PathVariable Integer id) {
+		return new ResponseEntity<>("sss", HttpStatus.OK);
+	}
+
 }
